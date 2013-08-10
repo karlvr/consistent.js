b52e02e3058a227765962e78a9f60272f51a791f
Karl von Randow <karl@xk72.com>
2013-08-10 22:49:36 +1200

    Test: fix Travis.ci test script

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Travis.ci test script
 
-./node_modules/.bin/karma start test/karma.conf.js --single-run --browsers PhantomJS,Firefox
+./node_modules/.bin/karma start test/karma/karma.conf.js --single-run --browsers PhantomJS,Firefox
 #./node_modules/.bin/karma start test/karma.min.conf.js --single-run --browsers PhantomJS,Firefox
